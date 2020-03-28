387fe6b4b3af2f1bbeb13e037466cd2a43edd3af
hogashi <hogashi@hatena.ne.jp>
2020-03-28 17:15:53 +0900

    fix a

--- 2019/a.rb
+++ 2019/a.rb
@@ -4,5 +4,5 @@ t.times{|x|
   n = gets.chomp
   a = n.gsub(/^[^4]*/) {|s| '' }.gsub(/[^4]/) {|s| '0' }.gsub(/4/) {|s| '1' }
   b = n.gsub(/4/) {|s| '3' }
-  puts "Case ##{x}: #{a} #{b}"
+  puts "Case ##{x + 1}: #{a} #{b}"
 }
